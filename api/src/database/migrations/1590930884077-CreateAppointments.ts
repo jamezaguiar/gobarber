8eb60b22d1e6407ebcc6330808dec1534c8828bd
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-05-31 11:45:25 -0300

    Configure default value for id

--- api/src/database/migrations/1590930884077-CreateAppointments.ts
+++ api/src/database/migrations/1590930884077-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1590930884077
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
