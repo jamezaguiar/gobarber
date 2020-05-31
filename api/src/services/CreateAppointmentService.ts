080542706fc5bd20e3ae0735ba3678ee3b6bd6b3
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-05-31 11:24:04 -0300

    Appointment service

---
+++ api/src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+/* eslint-disable class-methods-use-this */
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('There is already an appointment at this time');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+export default CreateAppointmentService;
