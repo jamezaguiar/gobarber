4df538917b05fcc1af05a6a6b7c80ceced415b8f
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-06-02 23:20:10 -0300

    Timestamps added

--- api/src/database/migrations/1590930884077-CreateAppointments.ts
+++ api/src/database/migrations/1590930884077-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1590930884077
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
