6603803c3279cb6ba29ba29961f08c14011b6060
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-06-06 14:58:06 -0300

    ID type changed to uuid

--- api/src/database/migrations/1590930884077-CreateAppointments.ts
+++ api/src/database/migrations/1590930884077-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1590930884077
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
