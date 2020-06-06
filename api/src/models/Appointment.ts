5de98ddc6962a55182cbe0b96da04328c3bc88b5
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-06-06 15:26:49 -0300

    relation between user and appointments

--- api/src/models/Appointment.ts
+++ api/src/models/Appointment.ts
@@ -4,7 +4,11 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -12,7 +16,13 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  // Relacionamento de muitos (agendamentos) para um (usuário)
+  @ManyToOne(() => User)
+  // Através da coluna provider_id
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
