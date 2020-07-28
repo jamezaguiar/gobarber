f308b4e8bec45fc307cb73a6bc74dec62d7d9caa
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-07-28 13:15:33 -0300

    Auth screen finalized

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,30 +1,86 @@
 import React from 'react';
-import { Image, Alert } from 'react-native';
+import {
+  Image,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-
-      <Button
-        onPress={() => {
-          Alert.alert('test', 'testing');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                Alert.alert(
+                  'Logon',
+                  'You will be redirected to dashboard page.',
+                );
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                Alert.alert(
+                  'Redirect',
+                  'You will be redirected to forgot password page.',
+                );
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+
+        <CreateAccountButton
+          onPress={() => {
+            Alert.alert(
+              'Redirect',
+              'You will be redirected to create account page.',
+            );
+          }}
+        >
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -9,7 +10,42 @@ export const Container = styled.View`
 
 export const Title = styled.Text`
   font-size: 24px;
-  color: #e4ede8;
+  color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 18px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+  color: #f4ede8;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  background: #392e38;
+  border-top-width: 1px;
+  border-color: #232129;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+  color: #ff9000;
+
+  margin-left: 8px;
 `;
