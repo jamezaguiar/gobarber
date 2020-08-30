459febbc88e0ad9736aa1d506822094486e1daa4
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-08-30 15:41:09 -0300

    SignUp page completed

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import {
+  Image,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInButtonText,
+} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                Alert.alert(
+                  'Logon',
+                  'You will be redirected to dashboard page.',
+                );
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+
+        <BackToSignInButton
+          onPress={() => {
+            Alert.alert('Redirect', 'You will be redirected to logon page.');
+          }}
+        >
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInButtonText>Voltar para logon</BackToSignInButtonText>
+        </BackToSignInButton>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
 export default SignUp;
