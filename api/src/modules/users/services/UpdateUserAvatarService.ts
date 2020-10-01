8edeb6c83739033d7a7b9a49f15aaff2a4f95483
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-10-01 09:27:18 -0300

    Storage Provider

--- api/src/modules/users/services/UpdateUserAvatarService.ts
+++ api/src/modules/users/services/UpdateUserAvatarService.ts
@@ -10,6 +10,8 @@ import uploadConfig from '@config/upload';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+
 interface IRequestDTO {
   user_id: string;
   avatarFilename: string;
@@ -20,6 +22,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({
@@ -33,15 +38,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
