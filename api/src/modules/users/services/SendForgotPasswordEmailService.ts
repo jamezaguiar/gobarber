38dfc5cde56acb714dc443f4d21ee04c879f20fa
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-10-07 17:31:51 -0300

    Generating token and some refactoring

--- api/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ api/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequestDTO {
   email: string;
@@ -17,6 +18,9 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequestDTO): Promise<void> {
@@ -25,6 +29,8 @@ class SendForgotPasswordEmailService {
     if (!user) {
       throw new AppError('User not found');
     }
+
+    await this.userTokensRepository.generate(user.id);
 
     await this.mailProvider.sendMail(
       email,
