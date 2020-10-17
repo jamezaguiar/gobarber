c404f63ffc4481444b73c79b9ba5f1b4c6376571
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-10-16 21:03:28 -0300

    Sending forgot password token on e-mail

--- api/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ api/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -21,30 +21,14 @@ export default class EtherealMailProvider implements IMailProvider {
   }
 
   public async sendMail(to: string, body: string): Promise<void> {
-    /* await this.client.sendMail({
+    const message = await this.client.sendMail({
       from: 'Equipe GoBarber <equipe@gobarber.com>',
       to,
       subject: 'Recuperação de Senha',
       text: body,
-    }); */
-
-    // Message object
-    const message = {
-      from: 'Equipe GoBarber <equipe@gobarber.com>',
-      to,
-      subject: 'Recuperação de Senha',
-      text: body,
-    };
-
-    this.client.sendMail(message, (err, info) => {
-      if (err) {
-        console.log(`Error occurred. ${err.message}`);
-        return process.exit(1);
-      }
-
-      console.log('Message sent: %s', info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
   }
 }
