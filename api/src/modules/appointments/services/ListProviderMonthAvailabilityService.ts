798a2d46a7ef8c02d1e91f5a48ccab56cf62c682
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-11-20 11:08:45 -0300

    ListProviderMonthAvailability service and test

---
+++ api/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,59 @@
+import { injectable, inject } from 'tsyringe';
+import { getDate, getDaysInMonth } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequestDTO {
+  provider_id: string;
+  year: number;
+  month: number;
+}
+
+type IResponseDTO = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+  }: IRequestDTO): Promise<IResponseDTO> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10, // Se um prestador tem mais de 10 agendamentos em um dia, sua agenda já está cheia.
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
