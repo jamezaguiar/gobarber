8d7f91de23482841e555bd2201e4f1fc059aa821
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-11-24 21:06:47 -0300

    Bug fixes

--- api/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ api/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -15,8 +15,12 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
     const appointment = await createAppointment.execute({
-      date: new Date(),
+      date: new Date(2020, 4, 10, 13),
       provider_id: 'provider',
       user_id: 'user',
     });
@@ -26,7 +30,11 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments in the same time', async () => {
-    const appointmentDate = new Date();
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    const appointmentDate = new Date(2020, 4, 10, 13);
 
     await createAppointment.execute({
       date: appointmentDate,
