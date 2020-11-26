c7e0284922773f8dcd0c328fbf40753d5c631161
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-11-26 14:51:27 -0300

    Notification module interfaces and schema

---
+++ api/src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,35 @@
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import Notification from '../schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    /*
+     * O segundo parâmetro de getMongoRepository() é o nome da configuração
+     * presente no ormconfig.json.
+     * Se não passarmos esse parâmetro, ele usará a configuração default,
+     * que é a usada pelo PostgreSQL, gerando um erro.
+     */
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    recipient_id,
+    content,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      recipient_id,
+      content,
+    });
+
+    await this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationsRepository;
