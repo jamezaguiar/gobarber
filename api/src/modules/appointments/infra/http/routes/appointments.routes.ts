29ef49145f88700c50b6f97e74e929750e1b9259
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-12-18 15:23:13 -0300

    Change route /schedule to /me

--- api/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ api/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -22,6 +22,6 @@ appointmentsRouter.post(
   }),
   appointmentsController.create,
 );
-appointmentsRouter.get('/schedule', providerAppointmentsController.index);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
